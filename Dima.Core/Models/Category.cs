1d903bc9c029e0fe2cf2ca6d71f22fc19e705fdb
Danilo Silva <danilo.silva@msn.com>
2025-03-01 11:22:09 -0300

    adiciona propriedades Id, Title, Description e UserId à classe Category

--- Dima.Core/Models/Category.cs
+++ Dima.Core/Models/Category.cs
@@ -2,5 +2,8 @@ namespace Dima.Core.Models;
 
 public class Category
 {
-    
+   public long Id { get; set; }
+   public string Title { get; set; } = string.Empty;
+   public string Description { get; set; } = string.Empty;
+   public string UserId { get; set; } = string.Empty;
 }
