da504eedc7be82ed0cfc6ee6603d1d4ac204b310
Danilo Silva <danilo.silva@msn.com>
2025-03-02 00:07:12 -0300

    adiciona endpoint para criação de transações com classes Request, Response e Handler

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,38 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPost(
+    "/v1/transacoes", (Request request, Handler handler) => handler.Handle(request))
+.WithName("Transações: Criar")
+.WithSummary("Cria uma nova transação")
+.Produces<Response>();
 
 app.Run();
+
+public class Request
+{
+    public string Titulo { get; set; } = string.Empty;
+    public DateTime DataCriacao { get; set; } = DateTime.Now;
+    public int Tipo { get; set; }
+    public decimal Valor { get; set; }
+    public long CategoriaId { get; set; }
+    public string UsuarioId { get; set; } = string.Empty;
+}
+
+public class Response
+{
+    public long Id { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+}
+
+public class Handler
+{
+    public Response Handle(Request request)
+    {
+        return new Response
+        {
+            Id = 4,
+            Titulo = request.Titulo,
+        };
+    }
+}
